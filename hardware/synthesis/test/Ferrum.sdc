8b5b178d0a4a3a0f664b2e4edd7384b4b19240a5
Tjark Petersen <tjark-petersen@gmx.de>
2022-03-07 21:13:09 +0100

    update quartus makefile

--- hardware/synthesis/test/Ferrum.sdc
+++
@@ -1,2 +0,0 @@
-create_clock -period 20  [get_ports {clock}]
-derive_clock_uncertainty
---
+++ hardware/synthesis/test/pinout.tcl
@@ -0,0 +1,3 @@
+set_location_assignment PIN_Y2 -to clock
+set_location_assignment PIN_G19 -to io_led
+set_location_assignment PIN_AB28 -to reset
