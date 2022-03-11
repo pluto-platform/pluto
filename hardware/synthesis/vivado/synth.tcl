333d38cfbd97c8c7c6ab11f7218a0f78e21666c2
Tjark Petersen <tjark-petersen@gmx.de>
2022-03-11 09:36:27 +0100

    remove old vivado scripts

--- hardware/synthesis/vivado/synth.tcl
+++
@@ -1,9 +0,0 @@
-read_verilog [ glob ../../build/Top.v ]
-read_xdc ./pinout.xdc
-synth_design -top Top -part xc7a35tcpg236-1
-opt_design
-place_design
-route_design
-report_timing_summary -file sta.rpt
-report_utilization -file util.rpt
-write_bitstream Ferrum.bit
