169555fe3a4da3e6d0e24a85f9973b1157050b65
Tjark Petersen <tjark-petersen@gmx.de>
2022-05-08 17:45:02 +0200

    wip

--- src/main/scala/cache/data/DataCache.scala
+++ src/main/scala/cache/data/DataCache.scala
@@ -1,9 +1,11 @@
 package cache.data
 
+import cache.Cache
+import charon.Tilelink
 import lib.Types._
 import chisel3._
 import chisel3.experimental.ChiselEnum
-import chisel3.util.{Decoupled, Valid}
+import chisel3.util.{Decoupled, DecoupledIO, Valid, ValidIO}
 import cores.lib.ControlTypes.{MemoryAccessResult, MemoryOperation}
 
 object DataCache {
@@ -13,17 +15,21 @@ object DataCache {
     val writeData = Vec(4, Byte())
     val byteEnable = Vec(4, Bool())
   }
+  class RequestIO extends DecoupledIO(new Request)
   class Response extends Bundle {
     val readData = Vec(4, Byte())
     val result = MemoryAccessResult()
   }
-}
-
-abstract class DataCache extends Module {
-
-  val io = IO(new Bundle {
+  class ResponseIO extends ValidIO(new Response)
+  class IO(dim: Cache.Dimension) extends Bundle {
     val request = Flipped(Decoupled(new DataCache.Request))
     val response = Valid(new DataCache.Response)
-  })
+    val tilelink = Tilelink.Agent.Interface.Requester(Tilelink.Parameters(4, 32, 2, Some(10), Some(10)))
+  }
+}
+
+abstract class DataCache(dim: Cache.Dimension) extends Module {
+
+  val io = IO(new DataCache.IO(dim))
 
 }
---
+++ src/main/scala/cache/data/DirectMapped.scala
@@ -0,0 +1,91 @@
+package cache.data
+
+import cache.Cache
+import cache.Cache.{AddressSplitter, LineInfo}
+import cache.data.DirectMapped.State
+import chisel3._
+import chisel3.experimental.ChiselEnum
+import chisel3.util.{Fill, RegEnable, UIntToOH, is, switch}
+import lib.Types.Byte
+import lib.util.{DataReducer, SeqToVecMethods}
+
+object DirectMapped {
+  object State extends ChiselEnum {
+    val Hit, Miss, Fill = Value
+  }
+}
+
+class DirectMapped(val dim: Cache.Dimension) extends DataCache(dim) {
+  implicit val d = dim
+  import dim.{lines, blockSize, wordsPerLine, Widths}
+
+  val blocks = SyncReadMem(lines, Vec(wordsPerLine * 4, Byte()))
+  val metas = SyncReadMem(lines, LineInfo())
+
+  val stateReg = RegInit(State.Hit)
+
+  val addressReg = RegInit(0.U(Widths.address.W))
+  val index = Mux(stateReg === State.Hit, io.request.bits.address.getIndex, addressReg.getIndex)
+  val fillPointerReg = RegInit((1.B +: Seq.fill(wordsPerLine - 1)(0.B)).toVec)
+  val requestPipe = RegNext(io.request.valid, 0.B)
+
+  val meta = metas.read(index)
+  val hit = meta.tag === addressReg.getTag && meta.valid
+  when(stateReg === State.Hit && (hit || !requestPipe)) { addressReg := io.request.bits.address }
+
+  val blockOffset = io.request.bits.address.getBlockOffset
+  val wordSelectorReg = RegEnable( // register a one-hot version of the block offset if cache is operating normally
+    UIntToOH(blockOffset).asBools.toVec,
+    Seq.fill(wordsPerLine)(0.B).toVec,
+    stateReg === State.Hit && (hit || !requestPipe)
+  )
+  io.response.bits.instruction := blocks.read(index).reduceWithOH(wordSelectorReg)
+
+
+  io.fillPort.address := addressReg.getTag ## addressReg.getIndex ## Fill(Widths.blockOffset + 2, 0.B)
+  io.fillPort.length := wordsPerLine.U
+  io.fillPort.fill := 0.B
+
+  io.request.ready := 0.B
+  io.response.valid := 0.B
+
+  switch(stateReg) {
+    is(State.Hit) {
+      stateReg := Mux(!hit && requestPipe, State.Miss, State.Hit)
+      io.request.ready := (requestPipe && hit) || !requestPipe
+      io.response.valid := requestPipe && hit
+    }
+    is(State.Miss) {
+      stateReg := State.Fill
+      io.request.ready := 0.B
+      io.response.valid := 0.B
+
+      metas.write(addressReg.getIndex, LineInfo(1.B, addressReg.getTag))
+
+    }
+    is(State.Fill) {
+      stateReg := State.Fill
+      io.fillPort.fill := 1.B
+      io.request.ready := 0.B
+      io.response.valid := 0.B
+
+      when(io.fillPort.valid) {
+
+        when(fillPointerReg.last) {
+          requestPipe := 1.B
+          stateReg := State.Hit
+        }
+
+        fillPointerReg := fillPointerReg.rotatedLeft
+        blocks.write(
+          addressReg.getIndex,
+          Seq.fill(wordsPerLine)(io.fillPort.data).toVec,
+          fillPointerReg
+        )
+
+      }
+
+    }
+  }
+
+}
